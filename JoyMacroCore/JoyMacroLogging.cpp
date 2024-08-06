a93b3b2c82e287ea38da8201ae78860dc0780753
Luis De Ávila <luis.deavila@urosario.edu.co>
2024-08-05 22:34:10 -0500

    Added proper release / debug messagebox printing

--- JoyMacroCore/JoyMacroLogging.cpp
+++ JoyMacroCore/JoyMacroLogging.cpp
@@ -4,16 +4,24 @@ void PrintScreenLogDebug(std::string message) {
 #if defined(_DEBUG)
 	std::cout << "[DEBUG] " + message << std::endl;
 #endif
-
-#if !defined(_DEBUG)
-	MessageBox(NULL, L"Debug Message", WStringUtils::ConvertUtf8ToWide(message).c_str(), MB_OK);
-#endif
 }
 
 void PrintScreenLogWarn(std::string message) {
+#if defined(_DEBUG)
 	std::cout << "[WARN] " + message << std::endl;
+#endif
+
+#if !defined(_DEBUG)
+	MessageBox(NULL, WStringUtils::ConvertUtf8ToWide(message).c_str(), L"JoyMacroCore WARNING", MB_OK | MB_ICONWARNING);
+#endif
 }
 
 void PrintScreenLogError(std::string message) {
+#if defined(_DEBUG)
 	std::cerr << "[ERROR] " + message << std::endl;
+#endif
+
+#if !defined(_DEBUG)
+	MessageBox(NULL, WStringUtils::ConvertUtf8ToWide(message).c_str(), L"JoymacroCore ERROR", MB_OK | MB_ICONERROR);
+#endif
 }
